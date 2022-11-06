66670006055c924d03e05ef98526f211afb5d33b
nayeon0731 <toothless1596@gmail.com>
2022-11-06 15:08:49 +0900

    query 수정

--- src/main.js
+++ src/main.js
@@ -7,36 +7,45 @@ import { async } from "@firebase/util";
 
 const Main = () => {
     const userCollectionRef = collection(fireStore, 'lecture');
-    const $lectureNames = document.querySelector('#lectureNames');
+    const $lectureList = document.querySelector('#lectureList');
+    const $main = document.querySelector('#main');
+
+    const lectureQuery = query(collection(fireStore, 'lecture'), where('dep', "==", "컴퓨터공학전공"));
 
     let lecName = [];
 
     useEffect(() => {
         const getUsers = async () => {
-            const data = await getDocs(userCollectionRef);
+            const data = await getDocs(lectureQuery);
             data.forEach((doc) => {
-                console.log(doc.id, " => ", doc.data());
-                console.log(doc.data().lecName)
-                lecName.push(doc.data().lecName)
+                console.log(doc.data().lecName);
+                //lecName.push(doc.data().lecName)
             })
-            console.log(lecName)
-            showLecture(lecName);
+            //showLecture(lecName);
         }
         getUsers();
     })
     
-    const showLecture = (lecName) => {
-        for(let i=0; i<=lecName.size; i++) {
-            let lectureNameList = document.createElement('lectureList');
-            lectureNameList.innerHTML = `<li>${lecName[i]}</li>`;
-            document.body.appendChild(lectureNameList);
+    //강의 목록 보여주는 함수
+    const showLecture = (name) => {
+        console.log('for문 시작 전')
+        for(let i=0; i<=50; i++) {
+            console.log('들어왔땁');
+            let lecItem = document.createElement('div');
+            let lecName = document.createElement('p');
+            let lecNameTxt = document.createTextNode(name[i]);
+
+            lecName.appendChild(lecNameTxt);
+            lecItem.appendChild(lecName);
+            $lectureList.appendChild(lecItem);
+            $main.appendChild($lectureList);
         }
     }
     
     return (
         <div id="main">
             mainpage
-            <div id="lectureNames">
+            <div id="lectureList">
             </div>
         </div>
     )
