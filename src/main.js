23c0134e873d906a6df51d78bbaedd478a10e5ce
nayeon0731 <toothless1596@gmail.com>
2022-11-07 19:44:25 +0900

    데이터 가져오기 성공

--- src/main.js
+++ src/main.js
@@ -1,56 +1,75 @@
-import React, { useId } from "react";
-import {fireStore} from "./Firebase"
+import React, { useCallback, useId, useRef, useState } from "react";
+import { fireStore } from "./Firebase";
 import { useEffect } from "react";
 import { collection, Firestore, getDocs } from "firebase/firestore";
 import { doc, query, where } from "firebase/firestore";
 import { async } from "@firebase/util";
+import userEvent from "@testing-library/user-event";
+
+// function Lecture({ lectureName }) {
+//   return <span>{lectureName}</span>;
+// }
 
 const Main = () => {
-    const userCollectionRef = collection(fireStore, 'lecture');
-    const $lectureList = document.querySelector('#lectureList');
-    const $main = document.querySelector('#main');
+  const $main = document.querySelector("#main");
 
-    const lectureQuery = query(collection(fireStore, 'lecture'), where('prof', "==", "김수균"));
+  const lectureQuery = query(
+    collection(fireStore, "lecture"),
+    where("prof", "==", "김수균")
+  );
 
-    let lecName = [];
+  useEffect(() => {
+    const getUsers = async () => {
+      let lecNames = new Array();
+      console.log(lecNames.length);
+      const data = await getDocs(lectureQuery);
+      console.log(data);
+      data.forEach((doc) => {
+        console.log(doc.data());
+        lecNames.push(doc.data().lecName);
+      });
+      console.log(lecNames.length);
+      showLecture(lecNames);
+    };
+    getUsers();
+  }, []);
 
-    useEffect(() => {
-        const getUsers = async () => {
-            const data = await getDocs(lectureQuery);
-            data.forEach((doc) => {
-                console.log(doc.data().lecName);
-                lecName.push(doc.data().lecName)
-                showLecture(lecName);
-            })
-        }
-        getUsers();
-    })
-    
-    //강의 목록 보여주는 함수
-    const showLecture = (name) => {
-        console.log('for문 시작 전')
-        for(let i=0; i<=name.length; i++) {
-            console.log('들어왔땁');
-            let lecItem = document.createElement('div');
-            let lecName = document.createElement('p');
-            let lecNameTxt = document.createTextNode(name[i]);
+  //강의 목록 보여주는 함수
+  const showLecture = (name) => {
+    const $lectureList = document.querySelector("#lectureList");
+    console.log("for문 시작 전");
+    for (let i = 0; i < name.length; i++) {
+      console.log("들어왔땁");
+      console.log(name[i]);
+      let lecItem = document.createElement("div");
+      console.log("div");
+      let lecName = document.createElement("p");
+      console.log("p");
+      let lecNameTxt = document.createTextNode(name[i]);
+      console.log("name");
 
-            lecName.appendChild(lecNameTxt);
-            lecItem.appendChild(lecName);
-            $lectureList.appendChild(lecItem);
-            $main.appendChild($lectureList);
+      lecName.appendChild(lecNameTxt);
+      console.log("app lecName");
+      lecItem.appendChild(lecName);
+      console.log("app lecItem");
 
-            console.log(name[i]);
-        }
+      $lectureList.appendChild(lecItem);
+      console.log("app leclist");
+
+      //   $main.appendChild($lectureList);
     }
-    
-    return (
-        <div id="main">
-            mainpage
-            <div id="lectureList">
-            </div>
-        </div>
-    )
-}
+  };
 
-export default Main
+  return (
+    <div id="main">
+      mainpage
+      <div id="lectureList">
+        {/* {lecNames.map((lecture) => (
+          <Lecture name={lecture.lectureName} />
+        ))} */}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
