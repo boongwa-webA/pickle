d384a5e7fb9d21f4e15fb558c7ffde3fc7dc45fc
nayeon0731 <toothless1596@gmail.com>
2022-11-11 07:04:32 +0900

    timeTable coloring

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import "./main.css";
 import { async } from "@firebase/util";
 import userEvent from "@testing-library/user-event";
 import { getAuth } from "firebase/auth";
+import { tab } from "@testing-library/user-event/dist/tab";
 
 const Main = () => {
   const lectureQuery = query(
@@ -25,9 +26,12 @@ const Main = () => {
   //기초교양 과목 쿼리
   //전공과목 탐색 쿼리
 
+  let uid;
+
   useEffect(() => {
-    const auth = getAuth();
-    console.log(auth);
+    //    const auth = getAuth();
+    //    uid = auth.currentUser.uid;
+    //    console.log(auth.currentUser.uid);
     const getLectureList = async () => {
       const data = await getDocs(lectureQuery);
       data.forEach((doc) => {
@@ -36,8 +40,62 @@ const Main = () => {
       });
     };
     getLectureList();
+    initTable();
   }, []);
 
+  //시간표 찾기
+
+  //시간표 바탕 그리기
+  const initTable = () => {
+    const tableWrapper = document.getElementById("tableWrapper");
+    tableWrapper.innerHTML = "";
+    const days = ["time", "월", "화", "수", "목", "금"];
+    for (let i = 1; i < 10; i++) {
+      let row = document.createElement("div");
+      row.setAttribute("class", "row");
+      row.setAttribute("id", "row" + i);
+      for (let j = 0; j < 6; j++) {
+        let col = document.createElement("div");
+        if (j == 0) {
+          col.innerText = "" + i + "교시";
+        }
+        col.setAttribute("class", "col");
+        col.setAttribute("id", days[j] + "" + i);
+        row.appendChild(col);
+      }
+      tableWrapper.appendChild(row);
+    }
+  };
+
+  const drawTable = (id) => {
+    let chop = id.split(" ");
+    let cTime = [];
+    let cDay = [];
+    for (let i = 0; i < chop.length; i++) {
+      if (chop[i][0] === "[") {
+        let last = chop[i].length - 1;
+        if (chop[i][last] != "]") {
+          cTime.push(chop[i][last]);
+          cDay.push(chop[i][last - 1]);
+        }
+      } else {
+        cTime.push(chop[i][1]);
+        cDay.push(chop[i][0]);
+      }
+    }
+
+    console.log(cDay);
+    console.log(cTime);
+
+    let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    for (let i = 0; i < cDay.length; i++) {
+      console.log(cDay[i] + cTime[i]);
+      let dayDiv = document.getElementById(cDay[i] + cTime[i]);
+      console.log(dayDiv);
+      dayDiv.innerText = "헐랭";
+      dayDiv.style.backgroundColor = color;
+    }
+  };
   const getEssSubject = async () => {
     const essData = await getDocs(essLectureQuery);
     essData.forEach((doc) => {
@@ -146,12 +204,16 @@ const Main = () => {
     let popBtnTxt = document.createTextNode("추가");
     popBtn.appendChild(popBtnTxt);
     popBtn.setAttribute("class", "pop_button");
+    popBtn.setAttribute("id", itemInfo);
     popBtnBox.appendChild(popBtn);
     lecItem.appendChild(popBtnBox);
 
     lecItem.addEventListener("mouseover", (e) => {
       showBtn(popBtn, 1);
-      popBtn.addEventListener("mousedown", console.log("isclicked"));
+    });
+    popBtn.addEventListener("click", (e) => {
+      console.log("isclicked");
+      drawTable(e.target.id);
     });
     lecItem.addEventListener("mouseleave", (e) => {
       showBtn(popBtn, 0);
@@ -225,33 +287,7 @@ const Main = () => {
             </button>
           </div>
         </div>
-        <div className="lecture_list">
-          {/*<div className="lecture_item">
-            <div className="item subject" id="lecture_subject">
-              수강과목
-            </div>
-            <div className="item prof" id="lecture_prof">
-              교수명
-            </div>
-            <div className="item time" id="lecture_time">
-              시간
-            </div>
-            <div className="lecture_details">
-              <div className="details" id="lecture_grade">
-                학년
-              </div>
-              <div className="details" id="lecture_category">
-                이수
-              </div>
-              <div className="details" id="lecture_hakjum">
-                학점
-              </div>
-              <div className="details" id="lecture_num">
-                수강번호
-              </div>
-            </div>
-          </div>*/}
-        </div>
+        <div className="lecture_list"></div>
       </div>
       <div className="main timetable" id="user_timetable">
         <div className="timetablename">
@@ -262,110 +298,15 @@ const Main = () => {
           <button className="btn edit">+</button>
         </div>
         <div className="timetable">
-          <table width="100%" height="200" cellSpacing="3">
-            <tbody>
-              <tr className="trHeight" align="center" bgcolor="white">
-                <th></th>
-                <td>월</td>
-                <td>화</td>
-                <td>수</td>
-                <td>목</td>
-                <td>금</td>
-                <td>토</td>
-                <td>일</td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>9</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>10</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>11</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>12</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>13</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>14</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>15</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>16</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              <tr align="center" bgcolor="white">
-                <th>17</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="header">
+            <div className="day"> </div>
+            <div className="day">월</div>
+            <div className="day">화</div>
+            <div className="day">수</div>
+            <div className="day">목</div>
+            <div className="day">금</div>
+          </div>
+          <div className="tableWrapper" id="tableWrapper"></div>
         </div>
       </div>
     </div>
